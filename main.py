17b4abab734a0f44853cb18cbf9a7e6120985f88
Tan Tran <trantan0012@gmail.com>
2024-08-17 21:53:03 -0500

    Adding Main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+#  Automated Backup Script: 
+# Create a script that automates the backup of specific 
+# files or directories, with options for scheduling 
+# and selecting backup locations. 
+# This showcases your ability to manage data securely.
+
+"""
+# PSEUDO CODE 
+"""
+
+"""
